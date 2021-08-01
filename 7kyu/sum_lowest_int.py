cb1d3d1db5f36a58e0211586480068d19a2f6128
Jaspar <j.cyxtus@gmail.com>
2021-08-01 17:39:31 +0000

    Solved Sum of two lowest positive integers

--- 7kyu/sum_lowest_int.py
+++ 7kyu/sum_lowest_int.py
@@ -5,3 +5,17 @@ For example, when an array is passed like [19, 5, 42, 2, 77], the output should 
 
 [10, 343445353, 3453445, 3453545353453] should return 3453455.
 """
+
+
+def sum_min(numbers):
+
+    return sum(numbers.pop(numbers.index(min(numbers))) for _ in range(2))
+
+
+def sum_min2(numbers):
+
+    return sum(sorted(numbers)[:2])
+
+
+print(sum_min([19, 5, 42, 2, 77]))
+print(sum_min2([19, 5, 42, 2, 77]))
