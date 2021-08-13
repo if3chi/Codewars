d5654da4ebf133c7ca8181d5f37aada83b613271
Jaspar <j.cyxtus@gmail.com>
2021-08-13 19:11:01 +0000

    Solved Range Extraction

--- 4kyu/range_extraction.py
+++ 4kyu/range_extraction.py
@@ -19,7 +19,21 @@ Courtesy of rosettacode.org
 
 
 def solution(args):
-    pass
+    out = []
+    beg = end = args[0]
+
+    for n in args[1:] + [""]:
+        if n != end + 1:
+            if end == beg:
+                out.append(str(beg))
+            elif end == beg + 1:
+                out.extend([str(beg), str(end)])
+            else:
+                out.append(str(beg) + "-" + str(end))
+            beg = n
+        end = n
+
+    return ",".join(out)
 
 
 print(
