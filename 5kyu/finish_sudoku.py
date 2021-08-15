3eb4a29b9f2f5cf874cc2e60d994692e15e82da6
Jaspar <j.cyxtus@gmail.com>
2021-08-15 23:30:10 +0000

    Solved Did I Finish my Sudoku

--- 5kyu/finish_sudoku.py
+++ 5kyu/finish_sudoku.py
@@ -48,10 +48,17 @@ Sudoku: http://en.wikipedia.org/wiki/Sudoku and http://www.sudokuessentials.com/
 """
 
 
-def done_or_not(board):  # board[i][j]
-    # your solution here
-    # ..
-    # return 'Finished!'
-    # ..
-    # or return 'Try again!'
-    pass
+def done_or_not(board):
+    rows = board
+    cols = [map(lambda x: x[i], board) for i in range(9)]
+    squares = [
+        board[i][j : j + 3] + board[i + 1][j : j + 3] + board[i + 2][j : j + 3]
+        for i in range(0, 9, 3)
+        for j in range(0, 9, 3)
+    ]
+
+    for clusters in (rows, cols, squares):
+        for cluster in clusters:
+            if len(set(cluster)) != 9:
+                return "Try again!"
+    return "Finished!"
